8c51ddc498c8da497686ccaac43de6835051fce4
lumanti-rgb <lumantimanandhar091@gmail.com>
2025-07-28 11:57:35 +0545

    testing

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -15,5 +15,5 @@ docker push "$ECR_REGISTRY/$ECR_REPOSITORY:$branch_name-latest"
 aws ssm send-command \
     --document-name "AWS-RunShellScript" \
     --targets "[{\"Key\":\"InstanceIds\",\"Values\":[\"$EC2_INSTANCE_ID\"]}]" \
-    --parameters "{\"commands\":[\"sudo su - root -c '/root/deployment/deployment_script_teamN-name.sh'\"]}" \
+    --parameters "{\"commands\":[\"sudo su - root -c '/root/deployment/deployment_script_team2-lumanti.sh'\"]}" \
     --region "$AWS_REGION"
